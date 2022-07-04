a1bbe643896289bcaa5cea5164c7136e6b0631ec
beastmodewatt <fictionfreeway@gmail.com>
2022-07-03 20:22:17 -0400

    Update finance-calculator.js

--- week-6/finance-calculator.js
+++ week-6/finance-calculator.js
@@ -6,11 +6,11 @@
 */
 
 export class FinanceCalculator {
-    static MONTHS_IN_YEAR = 12;
+    MONTHS_IN_YEAR = 12;
 
     calculateFutureValue(monthlyPayment, rate, years) {
-        console.log(MONTHS_IN_YEAR);
-        let months = parseFloat(years) * MONTHS_IN_YEAR;
+        console.log(this.MONTHS_IN_YEAR);
+        let months = parseFloat(years) * this.MONTHS_IN_YEAR;
         let interestRate = 1 + parseFloat(rate) / 100;
         let presentValue = parseFloat(monthlyPayment) * months;
         let futureValue = presentValue * (Math.pow(interestRate, months));
