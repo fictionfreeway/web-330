2ee7a605e3f4b8078926c1c5a55ce0008914151a
beastmodewatt <fictionfreeway@gmail.com>
2022-07-16 19:26:54 -0400

    test

--- week-8/watlington-whatabook2.js
+++ week-8/watlington-whatabook2.js
@@ -51,7 +51,40 @@ function getBook(e) {
 
 function buildHtmlString(res, format) {
     let tableString = "<table class='table' id='bookTable'><tr><th>ISBN</th><th>Title</th><th>Description</th><th>Pages</th><th>Authors</th></tr>";
+    let ulString = "";
     
+    for(let key in res) {
+        ulString += "<ul style='list-style-type: none;'>";
+
+        if(res.hasOwnProperty(key)) {
+            let authors = [];
+            if(res[key].details.authors) {
+                authors = res[key].details.authors.map(function(author){
+                    return author.name;
+                })
+            }
+
+            let book = {
+                isbn: res[key].details.isbn_13 | res[key].details.isbn_13 | res[key].details.isbn_10,
+                title: res[key].details.title,
+                description: res[key].details.subtitle | res[key].details.subtitle | "N/A",
+                pages: res[key].details.number_of_pages | res[key].details.number_of_pages | "N/A",
+                authors: authors.join(',')
+            }
+
+            ulString += `<li><b>ISBN:</b> ${book.isbn}</li><li><b>Title:</b> ${book.title}</li><li><b>Description:</b> ${book.description}</li><li>
+            <b>Pages:</b> ${book.pages}</li><li><b>Authors:</b> ${book.authors}`;
+            
+            tableString += `<tr><td data-value="ISBN"><a href="#" class="isbn-link">${book.isbn}</a></td><td data-value="Title">${book.title}</td>
+            <td data-value="Description">${book.description}</td><td data-value="Pages">${book.pages}</td><td data-value="Authors">${book.authors}</td></tr>`;
+        }
+    }
+    tableString += "</table>"
+    if(format == "table") {
+        return tableString;
+    } else {
+        return ulString;
+    }
 }
 
 function addIsbnClickEvents() {
